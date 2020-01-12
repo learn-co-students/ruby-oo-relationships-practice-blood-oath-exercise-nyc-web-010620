239d0025aa25a38639f835472c222aadafc7fd5f
alex.szeto <alex.szeto.a@gmail.com>
2020-01-12 00:35:10 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,11 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+herbalife = Cult.new("Herbalife", "Everywhere", 1990, "Buy our products")#(name, location, founding_year, slogan)
+adam = Follower.new("Adam", 26, "Do it")#(name, age, life_motto)
+bond = Bloodoath.new("today", herbalife, adam)
+
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
